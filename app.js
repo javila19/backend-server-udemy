3a6813aea98abd1540e831c78a4b7bf02c1a5203
javila19 <javila7970@gmail.com>
2019-09-29 18:00:54 -0300

    Usuario y token listo

--- app.js
+++ app.js
@@ -2,10 +2,29 @@
 
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 
 // Inicializar variables
 var app = express(); // defino el servidor express.
+
+
+// Body Parser
+//middleware: son funciones que se van a ejecutar siempre.
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
+
+
+
+// Importar rutas
+
+var appRoutes = require('./routes/app');
+var usuarioRoutes = require('./routes/usuario');
+var loginRoutes = require('./routes/login');
+
 
 
 // Conexión a la base de datos
@@ -19,14 +38,11 @@ mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) =
 
 
 // Rutas
-app.get('/', (req, res, next) => {
-    res.status(200).json({
-        ok: true,
-        mensaje: 'Petición realizada correctamente.'
-    });
-});
+//middleware: son funciones que se van a ejecutar siempre.
 
-
+app.use('/usuario', usuarioRoutes);
+app.use('/login', loginRoutes);
+app.use('/', appRoutes);
 
 // Escuchar peticiones
 app.listen(3000, () => {
---
+++ models/usuario.js
@@ -0,0 +1,27 @@
+var mongoose = require('mongoose');
+var uniqueValidator = require('mongoose-unique-validator'); // Plugin para validaciones de emails. Envía errores de forma más estética.
+
+var Schema = mongoose.Schema;
+
+// Objeto para controlar roles válidos
+
+var rolesValidos = {
+
+    values: ['ADMIN_ROLE', 'USER_ROLE'],
+    message: '{VALUE} no es un rol permitido'
+};
+
+var usuarioSchema = new Schema({
+
+    nombre: { type: String, required: [true, 'El nombre es necesario'] },
+    email: { type: String, unique: true, required: [true, 'El correo es necesario'] },
+    password: { type: String, required: [true, 'La contraseña es necesaria'] },
+    img: { type: String, required: false },
+    role: { type: String, required: true, default: 'USER_ROLE', enum: rolesValidos }
+
+});
+
+
+usuarioSchema.plugin(uniqueValidator, { message: 'El {PATH} debe de ser único' });
+
+module.exports = mongoose.model('usuario', usuarioSchema); // Para exportar el esquema.
