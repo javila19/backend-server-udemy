1c1efc8ba478c746a5d23b9daba90ef101303009
javila19 <javila7970@gmail.com>
2019-10-04 21:30:00 -0300

    Sección 12 terminada - Backend terminado

--- config/config.js
+++ config/config.js
@@ -1,1 +1,6 @@
 module.exports.SEED = '@este-es@-un-seed-dificil';
+
+
+
+// Google
+module.exports.CLIENT_ID = '729898119684-osje07urhbe2nnt1u496ntthcfqhvtd0.apps.googleusercontent.com';
---
+++ google-signin-demo/README.md
@@ -0,0 +1,3 @@
+# Google-Signin-demo
+
+Mini Front-End para hacer pruebas del backend con tokens de google sign in.
